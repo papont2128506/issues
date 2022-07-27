4af573c2c4c7b3751ba66e308718fd6b2cdc4455
Andrey Trusov <a.trusov@voskhod.com>
2022-07-27 17:55:34 +0400

    Set user for issue

--- src/main/java/com/company/issues/screen/issue/IssueBrowse.java
+++ src/main/java/com/company/issues/screen/issue/IssueBrowse.java
@@ -1,10 +1,21 @@
 package com.company.issues.screen.issue;
 
+import com.company.issues.entity.User;
+import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.ui.screen.*;
 import com.company.issues.entity.Issue;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Issue.browse")
 @UiDescriptor("issue-browse.xml")
 @LookupComponent("table")
 public class IssueBrowse extends MasterDetailScreen<Issue> {
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+
+    @Subscribe
+    public void onInitEntity(InitEntityEvent<Issue> event) {
+        User currentUser = (User) currentAuthentication.getUser();
+        event.getEntity().setAuthor(currentUser);
+    }
 }
