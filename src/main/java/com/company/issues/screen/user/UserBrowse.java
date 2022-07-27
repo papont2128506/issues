992b3a1a97c773bd4cf737edaefa93b0606267f2
Andrey Trusov <a.trusov@voskhod.com>
2022-07-27 17:09:35 +0400

    Init project

---
+++ src/main/java/com/company/issues/screen/user/UserBrowse.java
@@ -0,0 +1,12 @@
+package com.company.issues.screen.user;
+
+import com.company.issues.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+
+@UiController("User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
---
+++ src/test/java/com/company/issues/IssuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.issues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
